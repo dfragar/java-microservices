4977b9802cb0f91ffc286b3e070c23f3078da754
dfragar <dfragar.dev@gmail.com>
2025-02-17 17:52:04 +0100

    feat(loans): add microservice with Docker image build using Buildpacks

---
+++ 02-Docker/loans/src/main/java/com/dfragar/loans/LoansApplication.java
@@ -0,0 +1,40 @@
+package com.dfragar.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "David Franco",
+                        email = "correo@gmail.com",
+                        url = "https://github.com/dfragar"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://github.com/dfragar"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Loans microservice REST API Documentation",
+                url = "http://localhost:8090/swagger-ui/index.html"
+        )
+)
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
