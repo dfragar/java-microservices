03537022ac998562dc7057db5f16057afa47ade2
dfragar <dfragar.dev@gmail.com>
2025-02-27 12:10:26 +0100

    docs(cards): update OpenAPI definition in CardsApplication.java

--- 01-Spring-Boot/cards/src/main/java/com/dfragar/cards/CardsApplication.java
+++ 01-Spring-Boot/cards/src/main/java/com/dfragar/cards/CardsApplication.java
@@ -14,21 +14,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Cards microservice REST API Documentation",
-                description = "EazyBank Cards microservice REST API Documentation",
+                description = "Bank Cards microservice REST API Documentation",
                 version = "v1",
                 contact = @Contact(
-                        name = "Madan Reddy",
-                        email = "tutor@eazybytes.com",
-                        url = "https://www.eazybytes.com"
+                        name = "David Franco",
+                        email = "dfragar.dev@gmail.com",
+                        url = "https://github.com/dfragar"
                 ),
                 license = @License(
                         name = "Apache 2.0",
-                        url = "https://www.eazybytes.com"
+                        url = "https://github.com/dfragar"
                 )
         ),
         externalDocs = @ExternalDocumentation(
-                description = "EazyBank Cards microservice REST API Documentation",
-                url = "https://www.eazybytes.com/swagger-ui.html"
+                description = "Bank Cards microservice REST API Documentation",
+                url = "https://www.app.com/swagger-ui.html"
         )
 )
 public class CardsApplication {
